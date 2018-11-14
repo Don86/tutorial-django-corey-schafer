3d43a7485b05c1743675b1a390924891a48518c1
Don86 <dh.teng.86@gmail.com>
2018-11-14 17:02:21 +1100

    code drop

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# Import removed, since we no longer need it
+# Kept for posterity
+#from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+	{
+	"author":"CoreyMS",
+	"title":"Corey's blog post",
+	"content":"first post content",
+	"date_posted":"12-10-2018"
+	},
+	{
+	"author":"William",
+	"title":"William's blog post",
+	"content":"second post content",
+	"date_posted":"15-10-2018"
+	},
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h`>')
+	"""Return render instance, with input requst, and target template path
+	"""
+	# Pass the posts list of dictionaries to render()
+	context = {'posts':posts}
+	return render(request, 'blog/home.html', context)
 
 
+def about(request):
+	about_response = '<h1>About This Throwaway</h>'
+	return render(request, 'blog/about.html', {'title':"About"})
